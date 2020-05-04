8f485210fce12e80285186e71279729de2045c04
Matt Stroud <lixingke06@gmail.com>
2020-05-04 14:00:42 -0700

    refactor to accept HH:MM format input

--- clock.cs
+++ clock.cs
@@ -4,12 +4,12 @@ class Clock
 {
   static void Main()
   {
-    Console.WriteLine("Enter the current hour:");
-    double hour = double.Parse(Console.ReadLine());
-    Console.WriteLine("Enter the current minute:");
-    double minute = double.Parse(Console.ReadLine());
-    // 3:00 = 90
-    // m = 0 H = 90
+    Console.WriteLine("Enter the time (hh:mm)");
+    string time = Console.ReadLine();
+    string[] timeSeparated = time.Split(":");
+    double hour = double.Parse(timeSeparated[0]);
+    double minute = double.Parse(timeSeparated[1]);
+
     double totalMinutes = hour * 60 + minute;
     double minuteDegrees = minute * 6;
     double hourDegrees = (hour * 30) + (minute * 0.5);
@@ -25,21 +25,3 @@ class Clock
     }
   }
 }
-
-/*
-
-10:15
-
-h = 307.5
-m = 90
-
-7:36
-
-h = 210 + 18 = 228
-m = 216
-*/
-
-// 3:30
-// Hour = 360 /12 = 30 + (MINUTES * 0.5)
-// 180
-// 105  
